5c3961838e8d63a886cab0a0dcf82861afdfb336
Dexlo <92215938+dangdat182@users.noreply.github.com>
2025-06-18 16:26:50 +0700

    Update auto_deploy_monitor.sh

--- release/auto_deploy_monitor.sh
+++ release/auto_deploy_monitor.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Install Helm
 set -e
+
+# Install Helm vào thư mục local
 LATEST_VERSION=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
 curl -LO https://get.helm.sh/helm-${LATEST_VERSION}-linux-amd64.tar.gz
 tar -zxvf helm-${LATEST_VERSION}-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/helm
-rm -rf linux-amd64 helm-${LATEST_VERSION}-linux-amd64.tar.gz
+chmod +x linux-amd64/helm
+export PATH=$PATH:$(pwd)/linux-amd64  # Thêm helm vào PATH tạm thời
 helm version
 
 # Tạo namespace
-kubectl create namespace monitoring
+kubectl create namespace monitoring || true
 
 # Thêm Helm repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -17,21 +18,12 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Cài Prometheus
-helm install prometheus prometheus-community/prometheus \
-  --namespace monitoring
+helm install prometheus prometheus-community/prometheus --namespace monitoring
 
-# Cài Grafana với cấu hình từ values.yaml
+# Cài Grafana
 helm install grafana grafana/grafana \
   --namespace monitoring \
   --values grafana-values.yaml
 
 # Hiển thị địa chỉ Grafana
 kubectl get svc -n monitoring grafana
-
-#install network tools
-sudo apt update && sudo apt install iperf3 -y
-
-#Intall iperf3 curl wrk inside pod
-#apk update && apk add iperf3
-#apk update && apk add curl
-#apk update && apk add wrk 
